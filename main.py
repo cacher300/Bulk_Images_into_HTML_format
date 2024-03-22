366e0fc8bd2dcd20e794f6844bbe6e38218fd29c
Personal website <cacher300@gmail.com>
2024-03-21 23:15:54 -0300

    Finished product

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def split_and_format_images(folder_path):
+    all_files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    image_files = [file for file in all_files if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+
+    random.shuffle(image_files)
+
+    midpoint = len(image_files) // 2
+    first_half, second_half = image_files[:midpoint], image_files[midpoint:]
+
+    def format_filenames(names):
+        return [f'<img src="{{{{ url_for(\'static\', filename=\'{name}\') }}}}" loading="lazy"/>' for name in names]
+
+    first_half_formatted = format_filenames(first_half)
+    second_half_formatted = format_filenames(second_half)
+
+    with open('first_half.txt', 'w') as f1, open('second_half.txt', 'w') as f2:
+        f1.write('\n'.join(first_half_formatted))
+        f2.write('\n'.join(second_half_formatted))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+folder_path = r""
+split_and_format_images(folder_path)
+
+
